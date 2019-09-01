842b1bcc586f206c3e5b142ac81d7b2d51a2300f
Edoardo <edoardore@me.com>
2019-09-01 12:25:01 +0200

    Implementato DB che riceve risultati delle HITs

--- imageManager.py
+++ imageManager.py
@@ -6,6 +6,7 @@ import pickle
 aws_access_key_id = ''
 aws_secret_access_key = ''
 
+
 def upload_to_aws(local_file, bucket, s3_file):
     s3 = boto3.client('s3',
                       aws_access_key_id=aws_access_key_id,
@@ -19,9 +20,15 @@ def upload_to_aws(local_file, bucket, s3_file):
         print("Credentials not available")
         return False
 
+
 uploaded = []
 images = os.listdir("images")
+len = len(images)
+i = 0
 for img in images:
     if upload_to_aws("images/" + img, 'imagesformturk', img):
+        i += 1
         uploaded.append("https://imagesformturk.s3.eu-central-1.amazonaws.com/" + img)
+        percent = str(i * 100 / len)
+        print (percent + '%')
 pickle.dump(uploaded, open('imagesurl.p', 'wb'))
